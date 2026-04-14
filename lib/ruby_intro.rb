bfa49997ae6ed02870c7f0c96bf940ad49f3311a
ipidara <ishanipidara2026@u.northwestern.edu>
2026-04-13 23:13:05 -0500

    part 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,63 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  if arr.empty?
+    return sum
+  end
+  arr.each { |x| sum += x }
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum = 0
+  if arr.empty?
+    return sum
+  elsif arr.length == 1
+    return arr[0]
+  elsif
+    sorted = arr.sort 
+    sum = sorted[-2] + sorted[-1]
+    return sum
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.empty?
+    return false
+  end
+  arr.each do |x|
+    arr2 = arr.dup 
+    arr2.delete_at(arr2.index(x))
+    if arr2.include?(n - x)
+      return true
+    end
+  end
+  return false
 end
-
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false if s.empty?
+
+  consonants = ["a", "e", "i", "o", "u"]
+  s = s.downcase
+  s = s.split(//)
+  return false if consonants.include?(s[0])
+  return false unless s[0] =~ /[a-z]/
+  return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s == "0"
+  return false unless s.match? /\A[01]+\z/
+  s = s.split(//)
+  return false unless s[-1] == "0" && s[-2] == "0"
+  return true
 end
 
 # Part 3
